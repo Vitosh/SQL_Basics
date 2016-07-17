c6375f73992226b83a867ea5f48c927f42b28143
Vitosh Academy <vitosh.doynov@gmail.com>
2016-07-17 23:48:58 +0200

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -7,3 +7,6 @@ Visual Studio Data Tools -> needed for -> SQL Server Object Explorer
 http://stackoverflow.com/questions/30894755/ssdt-for-visual-studio-2015
 SSDT 2015 RTM for visual studio 2015 is found here.
 https://msdn.microsoft.com/en-us/mt186501.aspx
+
+Problem with diagram:
+DB>Properties>Files>Owner>sa
